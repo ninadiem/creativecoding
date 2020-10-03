0b55c475aa27cf74078406f3dffaf90db9861924
Nina Diem <diem.nina@hotmail.com>
2020-10-03 14:44:18 +0200

    Framerate

--- 01_Projekt_Wetter/sketch.js
+++ 01_Projekt_Wetter/sketch.js
@@ -2,10 +2,12 @@ var gui;
 
 var anz = 10;
 var distanz = 10;
+let fr = 30; //starting FPS
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipseMode(CENTER);
+  angleMode(DEGREES);
 
 
   noFill();
@@ -18,9 +20,12 @@ function draw() {
   strokeWeight(2);
   background(25, 25, 112, 20);
 
+
   stroke(120, 120, 250)
   if (mouseIsPressed) {
     for (let i = 1; i <= anz; i++) {
+      fr = 10; //starting FPS
+      frameRate(fr);
       strokeWeight(i/5);
       ellipse(mouseX, mouseY, distanz * i);
     }
