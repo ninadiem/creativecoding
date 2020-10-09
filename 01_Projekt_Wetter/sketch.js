84e7793b8443bbb895419e9e12b82760ec507d12
Nina Diem <diem.nina@hotmail.com>
2020-10-09 08:35:59 +0200

    Projekt verlinken

--- 01_Projekt_Wetter/sketch.js
+++ 01_Projekt_Wetter/sketch.js
@@ -9,7 +9,10 @@ function setup() {
   ellipseMode(CENTER);
   angleMode(DEGREES);
 
+  background(25, 25, 112);
 
+  fr = 10; //starting FPS
+    frameRate(10);
   noFill();
   sliderRange(0, 20, 2);
   gui = createGui('p5.gui');
@@ -18,15 +21,17 @@ function setup() {
 
 function draw() {
   strokeWeight(2);
-  background(25, 25, 112, 20);
+  background(25, 25, 112, 10);
 
 
   stroke(120, 120, 250)
   if (mouseIsPressed) {
     for (let i = 1; i <= anz; i++) {
-      fr = 10; //starting FPS
-      frameRate(fr);
-      strokeWeight(i/5);
+    /*  fr = 10; //starting FPS
+      frameRate(fr);*/
+      let alpha=map(i,1,anz,255,0);
+      stroke(120, 120, 250, alpha);
+      strokeWeight(i);
       ellipse(mouseX, mouseY, distanz * i);
     }
   }
