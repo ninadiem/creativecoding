2bbcb772304c67284c38ecfdccb7565219bc6e15
Nina Diem <diem.nina@hotmail.com>
2020-10-09 10:40:45 +0200

    Zusammenfuehren Raindrop + GUI anpassen

--- 01_Projekt_Wetter/sketch.js
+++ 01_Projekt_Wetter/sketch.js
@@ -1,39 +1,70 @@
 var gui;
 
-var anz = 10;
-var distanz = 10;
-let fr = 30; //starting FPS
+
+var Tropfengroesse = 1;
+var TropfengroesseMin = 1;
+var TropfengroesseMax = 7;
+var TropfengroesseStep = 2;
+
+var Wind = 10;
+var WindMin = 0;
+var WindMax = 70;
+var WindStep = 10;
+
+
+let x = random(windowWidth, windowHeight);
+
+
+let raindropsRadius = 50;
+let kreisdurchmesserMax = 300;
+let scale = 1.1;
+
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipseMode(CENTER);
   angleMode(DEGREES);
 
-  background(25, 25, 112);
-
-  fr = 10; //starting FPS
-    frameRate(10);
-  noFill();
-  sliderRange(0, 20, 2);
-  gui = createGui('p5.gui');
-  gui.addGlobals('anz', 'distanz');
-}
-
-function draw() {
-  strokeWeight(2);
-  background(25, 25, 112, 10);
+  // background(25, 25, 112);
 
 
   stroke(120, 120, 250)
-  if (mouseIsPressed) {
-    for (let i = 1; i <= anz; i++) {
-    /*  fr = 10; //starting FPS
-      frameRate(fr);*/
-      let alpha=map(i,1,anz,255,0);
-      stroke(120, 120, 250, alpha);
-      strokeWeight(i);
-      ellipse(mouseX, mouseY, distanz * i);
-    }
+  strokeWeight(5);
+  noFill();
+
+  frameRate(10);
+
+
+  gui = createGui('Regen');
+  gui.addGlobals('Tropfengroesse', 'Wind');
+}
+
+
+
+function draw() {
+  // background(25, 25, 112, 10);
+
+  background(0, 20);
+
+  raindropsRadius = raindropsRadius * scale;
+
+
+  if (raindropsRadius < kreisdurchmesserMax) {
+    ellipse(200, 200, raindropsRadius);
   }
 
 }
+
+
+
+// if (mouseIsPressed) {
+//   for (let i = 1; i <= anz; i++) {
+//   /*  fr = 10; //starting FPS
+//     frameRate(fr);*/
+//     let alpha=map(i,1,anz,255,0);
+//     stroke(120, 120, 250, alpha);
+//     strokeWeight(i);
+//     ellipse(mouseX, mouseY, distanz * i);
+//   }
+// }
