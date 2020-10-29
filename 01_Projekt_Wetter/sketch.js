e1aab062aed7029a1e8f5a43be687ddb9dcad129
Nina Diem <diem.nina@hotmail.com>
2020-10-29 15:31:44 +0100

    Korr + Info-Text einfügen

--- 01_Projekt_Wetter/sketch.js
+++ 01_Projekt_Wetter/sketch.js
@@ -1,4 +1,4 @@
-// PROBLEM: Niederschlag verändert Animations-Geschwindigkeit und nicht Anzahl Regentropfen
+//VARIABELN
 let key = '06e280b15621fb57f14de8e91c05e79e';
 let input, button;
 
@@ -27,7 +27,7 @@ let scale = 1.1;
 
 
 
-
+//SETUP
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ellipseMode(CENTER);
@@ -38,13 +38,14 @@ function setup() {
   loadJSON(url, gotWeather);
 
 
-
+//gui
   gui = createGui('Regen');
   gui.addGlobals('Niederschlag', 'Tropfengroesse');
 
 
+  //input feld
 
-  input = createInput('Stadt eingeben');
+  input = createInput('Stadt');
   input.position(30, 220);
   button = createButton('Suchen');
   button.position(input.x + input.width + 10, 220);
@@ -53,7 +54,7 @@ function setup() {
 
 
 
-  //regentropfen variablen füllen
+  //regentropfen x/y-position + radius variablen im array füllen – noch nichts wird gezeichnet
   for (let n = 0; n < NiederschlagMax; n++) {
     raindropsX[n] = random(width);
     raindropsY[n] = random(height);
@@ -61,20 +62,18 @@ function setup() {
   }
 
 
-
-
   background(0, 10);
 
 }
 
 
+
+
+//DRAW
 function draw() {
+  smooth();
 
   background(0, 10);
-
-
-
-  smooth();
 
   stroke(120, 120, 250);
   strokeWeight(2);
@@ -91,25 +90,24 @@ function draw() {
       raindropsRadius[n] = random(20);
     }
   }
+
+  //Box für Input
   strokeWeight(0);
-  fill(0);
+  fill(220);
   rect(20, 170, 245, 135);
+  fill(255);
+  rect(25, 175, 235, 125);
 
-
-    fill(220);
-    rect(20, 170, 245, 135);
-    fill(255);
-    rect(25, 175, 235, 125);
-
-    textSize(13);
-    fill(0);
-    text("Suche nach einer Stadt, um zu sehen, wie stark es dort regnet.", 30, 180, 200, 30);
-    // text(Niederschlag, 20, 170) & text('mm/h', 40, 170);
-    text('Die Niederschlagsmenge in ' + Stadt + ' beträgt ' + Niederschlag + ' mm/h', 30, 250, 200, 50);
+  textSize(13);
+  fill(0);
+  text("Suche nach einer Stadt, um zu sehen, wie stark es dort regnet.", 30, 180, 200, 30);
+  fill(120, 120, 250);
+  text('Die Niederschlagsmenge in ' + Stadt + ' beträgt ' + Niederschlag + ' mm/h', 30, 250, 200, 50);
 
 }
 
 
+//WETTER API
 function gotWeather(weather) {
   // Get the precip in mm
   Niederschlag = weather.current.precip * 10; // in mm!
@@ -118,7 +116,7 @@ function gotWeather(weather) {
   fill(0);
 }
 
-
+//JSON RELOAD
 function reloadJson() {
   // reload JSon kreiert eine neue url für die API mit dem Ort, den die User eingegeben haben
 
